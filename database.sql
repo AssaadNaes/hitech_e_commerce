f47e67fa4106d390fa0c5938a2a03231f85405c0
Assaad Naes <assaad.naes@mail.schwarz>
2024-09-25 11:59:55 +0200

    modified sql querys

--- database.sql
+++ database.sql
@@ -1,15 +1,16 @@
 -- Create the users table
 CREATE TABLE users (
-    email VARCHAR(255) PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
+    id SERIAL PRIMARY KEY
+    email VARCHAR(60),
+    name VARCHAR(50) NOT NULL,
     password VARCHAR(255) NOT NULL
 );
 
 -- Create the products table with image URL
 CREATE TABLE products (
     id SERIAL PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    image_url VARCHAR(255), -- URL to the image
+    name VARCHAR(50) NOT NULL,
+    image_url TEXT, -- URL to the image
     description TEXT,
     price DECIMAL(10, 2) NOT NULL
 );
@@ -19,7 +20,7 @@ CREATE TABLE carts (
     id SERIAL PRIMARY KEY,
     product_id INTEGER REFERENCES products(id),
     quantity_in_cart INTEGER NOT NULL CHECK (quantity_in_cart > 0),
-    user_id VARCHAR(255) REFERENCES users(email)
+    user_id SERIAL REFERENCES users(email)
 );
 
 -- Create an index on user_id in the carts table
